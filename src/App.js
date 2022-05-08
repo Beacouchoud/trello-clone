3b8d523f3f8ce4cc24f4d5294cbe9c6e87aa7bb6
Bea <34965770+Beacouchoud@users.noreply.github.com>
2022-05-08 18:27:59 +0200

    chore: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
+      <button onClick={() => props.addRandomTodo()}>Add todo</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({ state: state, hola: 44 });
+
+const mapDispatchToProps = (dispacth) => ({
+  addRandomTodo: () =>
+    dispacth({
+      type: "ADD_TODO",
+      payload: "borra esta tarea!",
+    }),
+});
+
+const connectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connectedApp;
