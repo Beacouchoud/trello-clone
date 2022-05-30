7171c70837cdf38cd26319e4495fa8b2d6ad1d5b
Bea <34965770+Beacouchoud@users.noreply.github.com>
2022-05-30 23:51:43 +0200

    fix: deploy route

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Board } from "./components/Board";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <Routes>
           <Route path="/" element={<App />}/>
